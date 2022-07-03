d1b9cf20fea8f01cf9d1ec98711c963319ac78f6
VanKertiss <DulchickIvan@gmail.com>
2022-07-03 16:50:02 +0300

    feat: add header layout

---
+++ src/components/Authorized/Authorized.tsx
@@ -0,0 +1,11 @@
+import HS from './Authorized.module.css';
+
+const Authorized = () => {
+    return (
+        <div className={HS.container}>
+            Authorized
+        </div>
+    )
+};
+
+export default Authorized;
---
+++ src/components/HelloPage/HelloPage.tsx
@@ -0,0 +1,11 @@
+import HS from './HelloPage.module.css';
+
+const HelloPage = () => {
+    return (
+        <div className={HS.container}>
+            HelloPage
+        </div>
+    )
+};
+
+export default HelloPage;
