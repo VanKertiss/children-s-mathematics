6d8fc289a599486b76059fa8733fc2f6221c5fa4
VanKertiss <DulchickIvan@gmail.com>
2022-07-04 00:24:19 +0300

    feat: add footer-layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
 import HS from './Footer.module.css';
+import footerLogo from '../../img/pngwing.com.png';
+import github from '../../img/github.png';
 
 const Footer = () => {
     return (
-        <div className={HS.container}></div>
+        <div className={HS.container}>
+            <Link to='/' className={HS.footerLogo}>
+                <img alt='' src={footerLogo} className={HS.footerLogoImg}></img>
+            </Link>
+            <div className={HS.developer}>Разработчик: Иван Дульчик</div>
+            <a href="https://github.com/VanKertiss" className={HS.githubLink}><img src={github} alt="" /> github: VanKertiss</a>
+        </div>
     )
 };
 
